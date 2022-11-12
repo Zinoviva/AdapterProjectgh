5562da6bbd6af414f677ef72549c9ffc918e816c
Yulia Zinovieva <Kiss-ka1-833@mail.ru>
2022-11-12 12:38:52 +0300

    'correct IntsCalculator.java'

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -8,18 +8,18 @@ public class IntsCalculator implements Ints {
     @Override
     public int sum(int arg0, int arg1) {  //сумма
         //считаем через target
-        return 0;
+        return (arg0 + arg1);
     }
 
     @Override
     public int mult(int arg0, int arg1) {  //умножение
         //считаем через target
-        return arg0;
+        return arg0*arg1;
     }
 
     @Override
     public int pow(int a, int b) {  //степень
         //считаем через target
-        return 0;
+        return (int) Math.pow(a, b);
     }
 }
